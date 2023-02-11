63431dea0f9cac69dc80a28120742a7135309321
KarthikBenki <karthikmc007@gmail.com>
2023-02-11 15:47:38 +0530

    add the button which will navigate to back to contact list

--- src/components/ContactDetail.jsx
+++ src/components/ContactDetail.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import user from'../images/user.avif'
- 
-const ContactDetail = (props) => {
-const {name,email} = props.location.state.contact;
-  return (
-    <div className='main'>
-        <div className="ui card centered">
-            <div className="image">
-                <img src={user} alt="user" />
-            </div>
-            <div className="content">
-                <div className="header">{name}</div>
-                <div className="description">{email}</div>
-            </div>
-        </div>
-    </div>
-  )
-}
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../images/user.avif";
 
-export default ContactDetail
+const ContactDetail = (props) => {
+  const { name, email } = props.location.state.contact;
+  return (
+    <div className="main">
+      <div className="ui card centered">
+        <div className="image">
+          <img src={user} alt="user" />
+        </div>
+        <div className="content">
+          <div className="header">{name}</div>
+          <div className="description">{email}</div>
+        </div>
+      </div>
+      <div className="center-div">
+        <Link to={`/`}>
+        <button className="ui button blue center">Back To Contact List</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ContactDetail;
